4000fbb8fc45b40517e59ece9215408d133e34a4
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-11-16 13:21:58 +0100

    Validacja poprzez IValidatableObject

--- BibliotekarzBlazor/Model/Person.cs
+++ BibliotekarzBlazor/Model/Person.cs
@@ -3,7 +3,7 @@ using BibliotekarzBlazor.Attributes;
 
 namespace BibliotekarzBlazor.Model;
 
-public class Person
+public class Person : IValidatableObject
 {
     [Required]
     public string Name { get; set; }
@@ -26,4 +26,35 @@ public class Person
     [Required(ErrorMessage = "Pole adres nie może być puste.")]
     [StringLength(25, MinimumLength = 15, ErrorMessage = "Długość adresu musi być pomiędzy 10 a 25 znaków.")]
     public string Address { get; set; }
+
+    public int? Day { get; set; }
+
+    public int? Month { get; set; }
+
+    public int? Year { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Day.HasValue && Month.HasValue && Year.HasValue)
+        {
+            bool isValid = true;
+
+            try
+            {
+                DateTime date = new DateTime(Year.Value, Month.Value, Day.Value);
+            }
+            catch 
+            {
+                isValid = false;
+            }
+
+            if (!isValid)
+                yield return new ValidationResult("Podana data jest nieprawidłowa", [nameof(Day), nameof(Month), nameof(Year)]);
+
+        }
+        else
+        {
+            yield return new ValidationResult("Brakuje danych", [nameof(Day), nameof(Month), nameof(Year)]);
+        }
+    }
 }
