a5b94c77bda9f8b27e2aece8389b9b4ef8506d69
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-11-16 12:27:08 +0100

    Validacja formularzy

---
+++ BibliotekarzBlazor/Model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BibliotekarzBlazor.Attributes;
+
+namespace BibliotekarzBlazor.Model;
+
+public class Person
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Range(18, 65, ErrorMessage = "Wprowadź wiek w zakresie od 18 do 65 lat.")]
+    public int Age { get; set; }
+
+    [Required(ErrorMessage = "Podaj adres email.")]
+    [EmailAddress(ErrorMessage = "Niepoprawny adres email.")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Podaj email w poprawnym formacie.")]
+    public string Mail { get; set; }
+
+    [Compare(nameof(Mail), ErrorMessage = "Adresy email różnią się.")]
+    public string ConfirmMail { get; set; }
+
+    [Required(ErrorMessage = "Podaj hasło.")]
+    [PasswordValidation(minLength: 8, maxLength: 30, true, true, true, true)]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Pole adres nie może być puste.")]
+    [StringLength(25, MinimumLength = 15, ErrorMessage = "Długość adresu musi być pomiędzy 10 a 25 znaków.")]
+    public string Address { get; set; }
+}
--- BibliotekarzBlazor/Pages/Home.razor.cs
+++ BibliotekarzBlazor/Pages/Home.razor.cs
@@ -1,54 +1,22 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using System.Web;
+using BibliotekarzBlazor.Model;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace BibliotekarzBlazor.Pages;
 
 public partial class Home
 {
-    string cos1;
-    private int count = 0;
+    private Person person { get; set; } = new Person();
 
-    [Parameter]
-    public int Id { get; set; }
-
-    [Parameter]
-    public string CosInnego { get; set; }
-
-    Dictionary<string, object> AdditionalAttributes;
 
     protected override void OnInitialized()
     {
-        AdditionalAttributes = new Dictionary<string, object>
-        {
-            ["Id"] = "Logos 2",
-            ["alt"] = "A logo of dotnet"
-        };
 
-        for (int i = 0; i < 5; i++)
-        {
-            AdditionalAttributes.Add("attribute_" + i, i);
-        }
+    }
 
-        Uri currentUri = new Uri(navigation.Uri);
-        var parameters = HttpUtility.ParseQueryString(currentUri.Query);
-
-        cos1 = parameters["cos1"];
-    }
-    private void IncrementCount(MouseEventArgs e)
+    private void AddPerson(EditContext context)
     {
-        count++;
-    }
-    private void NavigateToUrl(MouseEventArgs e)
-    {
-        navigation.NavigateTo("/counter");
-    }
-    private void NavigateToAbsoluteUrl(MouseEventArgs e)
-    {
-        navigation.NavigateTo("https://google.com");
-    }
-    private void NavigateWithReplaceHistory(MouseEventArgs e)
-    {
-        navigation.NavigateTo("/counter",false, true);
     }
 }
