f1bc9817242024afdd4685dbc543c02cdbbd9363
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-11-17 10:55:54 +0100

    Strona główna aplikacji Bibliotekarz

--- BibliotekarzBlazor/Pages/Home.razor.cs
+++ BibliotekarzBlazor/Pages/Home.razor.cs
@@ -2,21 +2,48 @@
 using Microsoft.AspNetCore.Components.Web;
 using System.Web;
 using BibliotekarzBlazor.Model;
+using BibliotekarzBlazor.Shared.DTOs;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace BibliotekarzBlazor.Pages;
 
 public partial class Home
 {
-    private Person person { get; set; } = new Person();
+    public string? FilterText { get; set; }
+
+    public List<BookDto> BookList { get; set; }
 
 
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
-
+        await GetData();
     }
 
-    private void AddPerson(EditContext context)
+    private async Task GetData()
     {
+        BookList = [
+        new()
+        {
+            Id = 1,
+            Title = "Harry Potter",
+            Author = "J.K. Rowling",
+            PageCount = 300,
+            IsBorrowed = false,
+            Borrower = null
+        },
+        new()
+        {
+            Id = 2,
+            Title = "Wiedźmin",
+            Author = "Andrzej Sapkowski",
+            PageCount = 400,
+            IsBorrowed = true,
+            Borrower = new()
+            {
+                Id = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski"
+            }
+        }];
     }
 }
