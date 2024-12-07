4254a5a53e1bec426f40dac8eaa15c98280ca724
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-12-07 15:23:12 +0100

    Dodanie IRepository

---
+++ Bibliotekarz.Data/Repositories/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace Bibliotekarz.Data.Repositories;
+
+public interface IRepository<T>
+{
+    void Add(T entity);
+    Task AddAsync(T entity);
+
+    void AddRange(IEnumerable<T> entities);
+    Task AddRangeAsync(IEnumerable<T> entities);
+
+    void Update(T entity);
+    Task UpdateAsync(T entity);
+
+    void Delete(T entity);
+    Task DeleteAsync(T entity);
+
+    void RemoveRange(IEnumerable<T>  entities);
+    Task RemoveRangeAsync(IEnumerable<T>  entities);
+
+    IQueryable<T> GetAll();
+
+    IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+
+    Task ApplyPatchAsync(T entity, params Patch[] patches);
+}
---
+++ Bibliotekarz.Data/Repositories/Patch.cs
@@ -0,0 +1,7 @@
+﻿namespace Bibliotekarz.Data.Repositories;
+
+public class Patch
+{
+    public required string PropertyName { get; set; }
+    public object? PropertyValue { get; set; }
+}
